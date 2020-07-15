a50f50f9d70f5b78bda1f354b693c7c9f19fd9c5
MD24 <matiasdaldi@gmail.com>
2020-07-15 13:22:46 -0400

    another fix for heroku

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(express.json());
 app.use("/public", express.static('./public'));
 
 app.get("/notes", (req, res) => { res.sendFile(path.join(__dirname, "/public/notes.html")) });
-app.get("/api/notes", (req, res) => { fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => { return res.json(JSON.parse(data)) }) });
+app.get("/api/notes", (req, res) => { fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => { return res.json(JSON.parse(data)) }) });
 app.get("*", (req, res) => { res.sendFile(path.join(__dirname, "/public/index.html")) });
 
 app.post("/api/notes", (req, res) => {
     const note = req.body;
-    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db.json"), "utf8", (err, data) => {
         const allNotes = JSON.parse(data);
         allNotes.push(note);
-        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(allNotes), (err, data) => {
+        fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(allNotes), (err, data) => {
             if (err) throw (err);
             return res.json(allNotes);
         })
@@ -27,13 +27,13 @@ app.post("/api/notes", (req, res) => {
 
 app.delete("/api/notes/:id", (req, res) => {
     const deleteNote = req.params.id;
-    fs.readFile(path.join(__dirname, "db/db/.json"), "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "/db/db/.json"), "utf8", (err, data) => {
         if (err) throw (err);
         const allNotes = JSON.parse(data);
         for (let i = 0; i < allNotes.length; i++) {
             if (deleteNote === allNotes[i].id) {
                 allNotes.splice(i, 1);
-                fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(allNotes), (err, data) => {
+                fs.writeFile(path.join(__dirname, "/db/db.json"), JSON.stringify(allNotes), (err, data) => {
                     if (err) throw (err);
                     return res.json(allNotes);
                 })
